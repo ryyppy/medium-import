810002b5122dfb2c3871e536623f29b72a2c5873
David Potgieter <david@weswingcats.com>
2017-02-26 15:43:37 +0100

    added additional params and setting defaults if not provided

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const path = require('path');
 const lodash = require('lodash');
 const mediumImport = require('./lib/mediumImport');
+const moment = require('moment');
 
 const DOTFILE = path.join(process.env.HOME, '.medium-import-rc');
 
@@ -20,6 +21,8 @@ cli
   .option('-cs, --clientSecret <clientSecret>', 'The registered application clientSecret')
   .option('-it, --integrationToken <token>', 'The registered integration token')
   .option('-f, --format <md|html>', 'File content format (md / html)')
+  .option('-p, --publishedAt <23-01-2017 12:00>', 'The optional backdated value - will only work if publishStatus is set to PUBLIC, and not saved as DRAFT.')
+  .option('-s, --publishStatus <PUBLIC|DRAFT>', 'Status of the post in Medium. Note that published date can only be overwritten when status is PUBLIC')
   .action(function(file, title) {
     noOp = false;
 
@@ -49,14 +52,18 @@ cli
       clientSecret: cli.clientSecret,
       integrationToken: cli.integrationToken,
       format: cli.format,
+      publishedAt: (cli.publishedAt) ? moment(cli.publishedAt).toISOString() : moment().toISOString(),
       content,
-      title
+      title,
+      publishStatus: cli.publishStatus || 'DRAFT',
     }, config);
 
     printRunInfo({
       format: arg.format,
       title,
       file,
+      publishedAt: arg.publishedAt,
+      publishStatus: arg.publishStatus,
       isUsingConfig
     });
 
@@ -73,6 +80,8 @@ function printRunInfo(arg){
   const format = arg.format;
   const title = arg.title;
   const file = arg.file;
+  const publishedAt = arg.publishedAt;
+  const publishStatus = arg.publishStatus;
   const isUsingConfig = arg.isUsingConfig;
 
   console.log('---');
@@ -83,6 +92,8 @@ function printRunInfo(arg){
   console.log(`Title: '${title}'`);
   console.log(`Format: '${format}'`);
   console.log(`File: '${file}'`);
+  console.log(`Published Date: '${publishedAt}'`);
+  console.log(`Published Status: '${publishStatus}'`);
   console.log('---');
 }
 
